25de8329df0942a9d53f5a3113296ade40dac4b0
Pavan Jakhu <pavanjakhu@gmail.com>
2014-12-02 14:42:40 -0500

    Animated Trap

--- Assets/Scripts/Traps.cs
+++ Assets/Scripts/Traps.cs
@@ -7,18 +7,18 @@ public class Traps : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     public void SetLocation(MazeCell cell)
     {
-        transform.localPosition = cell.transform.localPosition;
-        transform.localPosition += new Vector3(0f, 0.1f, 0f);
+        Debug.Log("Setting Location");
+        transform.position = cell.transform.localPosition;
+        Debug.Log(transform.position);
+        transform.position += new Vector3(0.0f, 0.1f, 0.0f);
     }
 }
