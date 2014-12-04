4c734004a8acc0541608c6afbb722e334e713173
Pavan Jakhu <pavanjakhu@gmail.com>
2014-12-03 20:41:17 -0500

    Implemented Character Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,68 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
+// Require these components when using this script
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    public float rotateSpeed = 3.0F;
-	public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-	public CharacterController controller;
+    [System.NonSerialized]
+    public float lookWeight;					// the amount to transition when using head look
 
-    private Vector3 moveDirection = Vector3.zero;
+    [System.NonSerialized]
+    public Transform enemy;						// a transform to Lerp the camera to during head look
+
+    public float animSpeed = 1.5f;				// a public setting for overall animator animation speed
+    public float lookSmoother = 3f;				// a smoothing setting for camera motion
+    public bool useCurves;						// a setting for teaching purposes to show use of curves
+
+
+    private Animator anim;							// a reference to the animator on the character
+    private AnimatorStateInfo currentBaseState;			// a reference to the current state of the animator, used for base layer
+    private AnimatorStateInfo layer2CurrentState;	// a reference to the current state of the animator, used for layer 2
+    private CapsuleCollider col;					// a reference to the capsule collider of the character
+
     private MazeCell currentCell;
 
     private Text bombText;
     private Text scoreText;
     private int numBombs;
     private int score;
+
+
+    static int idleState = Animator.StringToHash("Base Layer.Idle");
+    static int locoState = Animator.StringToHash("Base Layer.Locomotion");			// these integers are references to our animator's states
+    static int jumpState = Animator.StringToHash("Base Layer.Jump");				// and are used to check state for various actions to occur
+    static int jumpDownState = Animator.StringToHash("Base Layer.JumpDown");		// within our FixedUpdate() function below
+    static int fallState = Animator.StringToHash("Base Layer.Fall");
+    static int rollState = Animator.StringToHash("Base Layer.Roll");
+    static int waveState = Animator.StringToHash("Layer2.Wave");
+
+    void Start()
+    {
+        // initialising reference variables
+        numBombs = 0;
+        score = 0;
+        bombText = GameObject.Find("In-game Canvas(Clone)/Bombs").GetComponent<Text>();
+        scoreText = GameObject.Find("In-game Canvas(Clone)/Score").GetComponent<Text>();
+        anim = GetComponent<Animator>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+        float v = Input.GetAxis("Vertical");				// setup v variables as our vertical input axis
+        anim.SetFloat("Speed", v);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+        anim.SetFloat("Direction", h); 						// set our animator's float parameter 'Direction' equal to the horizontal input axis		
+        anim.speed = animSpeed;								// set the speed of our animator to the public variable 'animSpeed'
+        anim.SetLookAtWeight(lookWeight);					// set the Look At Weight - amount to use look at IK vs using the head's animation
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0);	// set our currentState variable to the current state of the Base Layer (0) of animation
+
+    }
 
     public void SetLocation(MazeCell cell)
     {
@@ -24,35 +71,7 @@ public class Player : MonoBehaviour
         transform.localPosition += new Vector3(0f, 0.5f, 0f);
     }
 
-	void Start () 
-	{
-		//controller = GetComponent<CharacterController>();
-        numBombs = 0;
-		//DontDestroyOnLoad(transform.gameObject);
-        bombText = GameObject.Find("In-game Canvas(Clone)/Bombs").GetComponent<Text>();
-        scoreText = GameObject.Find("In-game Canvas(Clone)/Score").GetComponent<Text>();
-	}
-
-    void Update()
-    {
-
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
-
-        if (controller.isGrounded)
-        {
-            //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-
-            moveDirection = transform.TransformDirection(moveDirection);
-
-            moveDirection *= speed;
-        }
-
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
-    }
-
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    void OnCollisionEnter(Collision hit)
     {
         switch (hit.gameObject.tag)
         {
